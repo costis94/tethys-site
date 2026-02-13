9edc29848233d14f7420c9ae549331cdf620164b
costis <costis0594@gmail.com>
2026-02-13 16:53:45 +0200

    multilingual added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 
+import react from "@astrojs/react";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 });
